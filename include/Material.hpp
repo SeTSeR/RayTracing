9643f4c27a128de6e2b50488b3669945d174106b
Sergey Makarov <setser200018@gmail.com>
2020-03-17 20:35:27 +0300

    Add reflection and refraction coefficients to material description

--- include/Material.hpp
+++ include/Material.hpp
@@ -7,26 +7,32 @@ template<typename T>
 class Material {
 private:
         Vec<3, T> diffuse_color;
-        Vec<2, T> albedo;
+        Vec<4, T> albedo;
         T specular_exponent;
 public:
-        Material(): diffuse_color(), albedo(Vec<2, T>({(T)1., (T)0.})), specular_exponent() {};
-        Material(Vec<3, T> &&color, Vec<2, T> &&albedo, T specular_exponent):
+        Material(): diffuse_color(), albedo(Vec((T)1., 0., 0., 0.)), specular_exponent() {};
+        Material(Vec<3, T> &&color, Vec<4, T> &&albedo, T specular_exponent):
                 diffuse_color(color), albedo(albedo), specular_exponent(specular_exponent) {};
         const Vec<3, T>& getDiffuseColor() const {
                 return diffuse_color;
         }
-        const Vec<2, T>& getAlbedo() const {
+        const Vec<4, T>& getAlbedo() const {
                 return albedo;
         }
         const T getSpecularExponent() const {
                 return specular_exponent;
         }
         static Material<T> ivory() {
-                return Material(Vec<3, T>((T)0.4, (T)0.4, (T)0.3), Vec<2, T>((T)0.6, (T)0.3), 50.);
+                return Material(Vec((T)0.4, 0.4, 0.3), Vec((T)0.6, 0.3, 0.1, 0.0), 50.);
         }
         static Material<T> red_rubber() {
-                return Material(Vec<3, T>((T)0.3, (T)0.1, (T)0.1), Vec<2, T>((T)0.9, (T)0.1), 10.);
+                return Material(Vec((T)0.3, 0.1, 0.1), Vec((T)0.9, 0.1, 0.1, 0.8), 10.);
+        }
+        static Material<T> glass() {
+                return Material(Vec((T)0.6, 0.7, 0.8, Vec((T)0.0, 0.5, 0.1, 0.8), 125.));
+        }
+        static Material<T> mirror() {
+                return Material(Vec((T)1.0, 1.0, 1.0), Vec((T)0.0, 10.0, 0.8, 0.0), 1425.);
         }
 };
 
