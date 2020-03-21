11578dcd719ca73c75545284b680e9128789726a
Sergey Makarov <setser200018@gmail.com>
2020-03-21 22:08:28 +0300

    Return old coefficients

--- include/Material.hpp
+++ include/Material.hpp
@@ -30,13 +30,13 @@ public:
                 return Material(Vec((T)0.4, 0.4, 0.3), Vec((T)0.6, 0.3, 0.1, 0.0), 50., 1.0);
         }
         static Material<T> red_rubber() {
-                return Material(Vec((T)0.3, 0.1, 0.1), Vec((T)0.8, 0.1, 0.1, 0.0), 10., 1.0);
+                return Material(Vec((T)0.3, 0.1, 0.1), Vec((T)0.9, 0.1, 0.1, 0.0), 10., 1.0);
         }
         static Material<T> glass() {
-                return Material(Vec((T)0.6, 0.7, 0.8), Vec((T)0.0, 0.38, 0.08, 0.54), 125., 1.5);
+                return Material(Vec((T)0.6, 0.7, 0.8), Vec((T)0.0, 0.5, 0.1, 0.8), 125., 1.5);
         }
         static Material<T> mirror() {
-                return Material(Vec((T)1.0, 1.0, 1.0), Vec((T)0.0, 0.92, 0.08, 0.0), 1425., 1.0);
+                return Material(Vec((T)1.0, 1.0, 1.0), Vec((T)0.0, 10.0, 0.8, 0.0), 1425., 1.0);
         }
         static Material<T> medium_gray() {
                 return Material(Vec((T)0.454902f, 0.454902, 0.454902), Vec((T)0.8, 0.1, 0.1, 0.0), 12.8, 1.0);
